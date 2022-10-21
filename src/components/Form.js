494e612f13dbd2ce3178856980ed67fcea181733
srhimel <himusr@gmail.com>
2022-10-21 16:38:54 +0600

    add transaction done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,90 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { createTransaction } from '../features/transaction/transactionSlice'
+
 export default function Form() {
-    return (
-        <div className="form">
-            <h3>Add new transaction</h3>
+  const [name, setName] = useState('')
+  const [type, setType] = useState('')
+  const [amount, setAmount] = useState('')
+  const dispatch = useDispatch()
+  const { isLoading, isError } = useSelector((state) => state.transaction)
 
-            <div className="form-group">
-                <label for="transaction_name">Name</label>
-                <input
-                    type="text"
-                    name="transaction_name"
-                    placeholder="My Salary"
-                />
-            </div>
+  const handleCreate = (e) => {
+    e.preventDefault()
+    dispatch(
+      createTransaction({
+        name,
+        type,
+        amount: Number(amount)
+      })
+    )
+  }
 
-            <div className="form-group radio">
-                <label for="transaction_type">Type</label>
-                <div className="radio_group">
-                    <input
-                        type="radio"
-                        value="income"
-                        name="transaction_type"
-                        checked
-                    />
-                    <label for="transaction_type">Income</label>
-                </div>
-                <div className="radio_group">
-                    <input
-                        type="radio"
-                        value="expense"
-                        name="transaction_type"
-                        placeholder="Expense"
-                    />
-                    <label for="transaction_type">Expense</label>
-                </div>
-            </div>
+  return (
+    <div className='form'>
+      <h3>Add new transaction</h3>
 
-            <div className="form-group">
-                <label for="transaction_amount">Amount</label>
-                <input
-                    type="number"
-                    placeholder="300"
-                    name="transaction_amount"
-                />
-            </div>
-
-            <button className="btn">Add Transaction</button>
-
-            <button className="btn cancel_edit">Cancel Edit</button>
+      <form onSubmit={handleCreate}>
+        <div className='form-group'>
+          <label>Name</label>
+          <input
+            type='text'
+            name='name'
+            required
+            placeholder='enter title'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    );
+
+        <div className='form-group radio'>
+          <label>Type</label>
+          <div className='radio_group'>
+            <input
+              required
+              type='radio'
+              value='income'
+              name='type'
+              checked={type === 'income'}
+              onChange={(e) => setType('income')}
+            />
+            <label>Income</label>
+          </div>
+          <div className='radio_group'>
+            <input
+              type='radio'
+              value='expense'
+              name='type'
+              placeholder='Expense'
+              checked={type === 'expense'}
+              onChange={(e) => setType('expense')}
+            />
+            <label>Expense</label>
+          </div>
+        </div>
+
+        <div className='form-group'>
+          <label>Amount</label>
+          <input
+            type='number'
+            required
+            placeholder='enter amount'
+            name='amount'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+
+        <button disabled={isLoading} className='btn' type='submit'>
+          Add Transaction
+        </button>
+
+        {!isLoading && isError && (
+          <p className='error'>There was an error occured</p>
+        )}
+      </form>
+
+      <button className='btn cancel_edit'>Cancel Edit</button>
+    </div>
+  )
 }
